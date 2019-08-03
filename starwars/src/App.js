94563fc43b81298e94d1c2921f76b7dc0dbc191d
lizdoyle <lizdoyle1121@gmail.com>
2019-08-03 13:56:47 -0400

    beginning commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import StarWars from "./components/StarWars";
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,9 +11,11 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <StarWars />
     </div>
   );
 }
