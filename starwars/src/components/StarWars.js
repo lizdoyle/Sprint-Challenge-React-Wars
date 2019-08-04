0d8de733c7ac901f9c5d6c2f6fda1ba716d3300a
lizdoyle <lizdoyle1121@gmail.com>
2019-08-04 16:37:28 -0400

    everything appears now to style :)

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -4,30 +4,31 @@ import {Card, Feed} from "semantic-ui-react";
 import './StarWars.scss';
 
 
-const StarWars = (props) => {
+const StarWars = (props) => (
 
             <Card className="swChar">
-                <Card.Header className="swHeader">{props.name}</Card.Header>
-               
-                height={props.height}
-                mass={props.mass}
-                color={props.hair_color}
-                skin={props.skin_color}
-                eyes={props.eye_color}
-                gender={props.gender}
-                bday={props.birth_year}
-                species={props.species}
-                homeworld={props.homeworld}
-                films={props.films}
-                vehicles={props.vehicles}
-                starships={props.starships}
-                created={props.created}
-                edited={props.edited}
-                url={props.url}
-                
-                
+                <Card.Header className="swHeader"> {props.name}</Card.Header>
+                    <Feed className="swFeed">
+                        <Feed.Content>Height: {props.height} </Feed.Content>
+                        <Feed.Content> Mass: {props.mass}</Feed.Content>
+                        <Feed.Content> Hair Color: {props.hair_color}</Feed.Content>
+                        <Feed.Content> Skin Color: {props.skin_color}</Feed.Content>
+                        <Feed.Content> Eye Color: {props.eye_color}</Feed.Content>
+                        <Feed.Content> gender: {props.gender}</Feed.Content>
+                        <Feed.Content>Birth Year: {props.birth_year} </Feed.Content>
+                        <Feed.Content>Species: {props.species} </Feed.Content>
+                        <Feed.Content>Homeworld: {props.homeworld} </Feed.Content>
+                        <Feed.Content>Films: {props.films} </Feed.Content>
+                        <Feed.Content>Vehicles: {props.vehicles}</Feed.Content>
+                        <Feed.Content>Starships: {props.starships}</Feed.Content>
+                        <Feed.Content>Created: {props.created}</Feed.Content>
+                        <Feed.Content>Edited: {props.edited}</Feed.Content>
+                        <Feed.Content>URL: {props.url}</Feed.Content>
+
+                    
+                    </Feed>
                 </Card>
        
-      }
+)
 
 export default StarWars
